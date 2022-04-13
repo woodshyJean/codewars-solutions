c4d8c5ef34e3e9932106619665905b1f5109e668
ac3bk <85188769+ac3bk@users.noreply.github.com>
2022-04-13 18:05:05 -0400

    Update Does my number look big in this?.js

--- javascript/Does my number look big in this?.js
+++ javascript/Does my number look big in this?.js
@@ -1,4 +1,4 @@
-A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+/*A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
 
 For example, take 153 (3 digits), which is narcisstic:
 
@@ -11,7 +11,7 @@ The Challenge:
 
 Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
 
-Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.*/
 
 
 
