4c49057c212e50fc8cf5223889725f902efe27ac
ac3bk <85188769+ac3bk@users.noreply.github.com>
2022-04-13 18:04:26 -0400

    Update Number of People in the Bus.js

--- javascript/Number of People in the Bus.js
+++ javascript/Number of People in the Bus.js
@@ -1,4 +1,4 @@
-There is a bus moving in the city, and it takes and drop some people in each bus stop.
+/*There is a bus moving in the city, and it takes and drop some people in each bus stop.
 
 You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
 
@@ -8,7 +8,7 @@ Take a look on the test cases.
 
 Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
 
-The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.*/
 
 
 /////////////////////////////////////////// solution /////////////////////////////////////////////
