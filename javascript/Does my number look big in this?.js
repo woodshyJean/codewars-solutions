a8eab4549fa7c5fa60c33e7478b5b6061a7f20ca
ac3bk <85188769+ac3bk@users.noreply.github.com>
2022-04-13 17:56:09 -0400

    Update Does my number look big in this?.js

--- javascript/Does my number look big in this?.js
+++ javascript/Does my number look big in this?.js
@@ -3,6 +3,7 @@ A Narcissistic Number is a positive number which is the sum of its own digits, e
 For example, take 153 (3 digits), which is narcisstic:
 
     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
 and 1652 (4 digits), which isn't:
 
     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
