38883e41dbddaead673a775b2098609e1ebc7bfb
ac3bk <85188769+ac3bk@users.noreply.github.com>
2022-04-13 18:04:46 -0400

    Update Categorize New Member.js

--- javascript/Categorize New Member.js
+++ javascript/Categorize New Member.js
@@ -1,4 +1,4 @@
-The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+/*The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
 To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
@@ -10,7 +10,7 @@ Output will consist of a list of string values (in Haskell: Open or Senior) stat
 
 Example
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
-output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]*/
 
 
 ////////////////////////////////////////// solution ////////////////////////////////////////////////////
